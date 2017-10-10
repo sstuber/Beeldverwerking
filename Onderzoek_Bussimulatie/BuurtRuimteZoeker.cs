216c27e17a7a8c391af2b6baefbf7b7954bc7625
sstuber <stanvanmeerendonk@gmail.com>
2017-10-10 20:17:03 +0200

    Add moveBus buurtruimte

--- Onderzoek_Bussimulatie/BuurtRuimteZoeker.cs
+++ Onderzoek_Bussimulatie/BuurtRuimteZoeker.cs
@@ -17,11 +17,48 @@ namespace Onderzoek_Bussimulatie
             double randomDouble = rnd.NextDouble();
             BuurtRuimte ruimte;
 
-            if (randomDouble < 0.5)
+            if (randomDouble < 0.33)
                 ruimte = AddBusBuurtRuimte(solution);
+            else if (randomDouble < 0.66)
+                ruimte = RemoveBusBuurtRuimte(solution);
             else
-                ruimte = RemoveBusBuurtRuimte(solution); 
+                ruimte = MoveBusBuurtRuimte(solution);
             return ruimte;
+        }
+
+        public BuurtRuimte MoveBusBuurtRuimte(Solution s)
+        {
+            // remove bus code
+            List<int> busPlaces = new List<int>();
+
+            for (int i = 0; i < s.busDistribution.Length; i++)
+                if (s.busDistribution[i])
+                    busPlaces.Add(i);
+
+            if (busPlaces.Count == 0)
+                return new Nullruimte();
+
+            int oldplaceIndex = rnd.Next(busPlaces.Count);
+            int oldplace = busPlaces[oldplaceIndex];
+
+            int newBusPlace = rnd.Next(s.peopleDistribution.Length);
+
+            // add bus code
+            int counter = 0;
+
+            while (s.busDistribution[newBusPlace] ||
+                (newBusPlace + 1 < s.peopleDistribution.Length && s.busDistribution[newBusPlace + 1]) ||
+                (newBusPlace > 0 && s.busDistribution[newBusPlace - 1]))
+            {
+                newBusPlace = rnd.Next(s.peopleDistribution.Length);
+
+                counter++;
+
+                if (counter > 10)
+                    return new Nullruimte();
+            }
+
+            return new MoveBus(s, oldplace, newBusPlace);
         }
 
         public BuurtRuimte AddBusBuurtRuimte(Solution s)
---
+++ Onderzoek_Bussimulatie/DataTypes/MoveBus.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onderzoek_Bussimulatie.DataTypes
+{
+    class MoveBus : BuurtRuimte
+    {
+        private int _oldPlace;
+        private int _newPlace;
+        private int _newScore;
+
+        public MoveBus(Solution solution, int oldPlace, int newPlace)
+        {
+            if (!solution.busDistribution[oldPlace])
+                throw new Exception("no bus existed in this place ");
+
+            if (solution.busDistribution[newPlace])
+                throw new Exception("bus already existed in new place");
+
+            _oldPlace = oldPlace;
+            _newPlace = newPlace;
+        }
+
+        public override void AcceptNewSolution(Solution solution)
+        {
+
+            solution.busDistribution[_oldPlace] = false;
+            solution.busDistribution[_newPlace] = true;
+            solution.solutionScore = _newScore;
+            
+        }
+
+        public override int GetDifference(Solution solution)
+        {
+            bool[] newDistribution = (bool[]) solution.busDistribution.Clone();
+            newDistribution[_oldPlace] = false;
+            newDistribution[_newPlace] = true;
+
+            _newScore = Simulation.CompleteSimulation(solution.peopleDistribution, newDistribution);
+
+            return _newScore - solution.solutionScore;
+        }
+    }
+}
