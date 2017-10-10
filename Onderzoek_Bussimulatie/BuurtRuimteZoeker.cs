3e2233cf2b974d6a947c2a4738f48ed184940efd
sstuber <stanvanmeerendonk@gmail.com>
2017-10-10 12:34:26 +0200

    Add Striped down framework from Optimalisering

---
+++ Onderzoek_Bussimulatie/BuurtRuimteZoeker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onderzoek_Bussimulatie
+{
+    class BuurtRuimteZoeker
+    {
+        public static Random rnd = new Random();
+
+        public BuurtRuimte ZoekBuurtRuimte(Solution solution)
+        {
+            return null;
+        }
+    }
+}
---
+++ Onderzoek_Bussimulatie/DataTypes/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Onderzoek_Bussimulatie
+{
+    class Solution
+    {
+        public int solutionScore = 0;
+        public int[] peopleDistribution;
+
+        public Solution(int[] peopleDistribution)
+        {
+            this.peopleDistribution = peopleDistribution;
+        }
+
+
+        public Solution Copy()
+        {
+            return this;
+        }
+    }
+}
